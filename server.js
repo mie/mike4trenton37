dcbae368f50860167ac504a1c42f8830fe7f5c01
Mikhail Mukhin <heavisome@gmail.com>
2022-05-29 22:29:40 +0300

    data server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const favicon = require('express-favicon');
+const path = require('path');
+const {GetKey, DataClient} = require("./lib/API")
+const ArgParser = require("./lib/argParser")
+
+const app = express();
+const port = process.env.PORT || 8080
+app.use(favicon(__dirname + '/build/favicon.ico')); 
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+const api_key = process.env.TRENTON37_API_KEY //new GetKey(filename).read_key()
+const cl = new DataClient()
+
+app.get("/data", (req, res) => {
+	cl.fetch(api_key).then(data => res.send(data)).catch(err => res.send({error: err}))
+});
+
+app.get('/*', function (req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`)
+})
